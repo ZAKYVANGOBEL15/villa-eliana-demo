951f28d5bb862cc0078166bb3fb1aa497c6a5cbb
ZAKYVANGOBEL15 <zakyvangobel15@gmail.com>
2026-06-07 22:15:25 +0800

    fix: update base path to repository name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/villa-eliana-demo/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
